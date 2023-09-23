6f912e77ce343857aee683a983d1aba60b9c076a
Vijay <vijay.s@eminds.ai>
2023-09-23 15:05:45 +0530

    send otp and reset password changes are done

---
+++ slackclown/src/main/java/com/example/slackclown/dto/EmailDto.java
@@ -0,0 +1,16 @@
+package com.example.slackclown.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDto {
+
+
+    private String email;
+}
--- slackclown/src/main/java/com/example/slackclown/service/UserService.java
+++ slackclown/src/main/java/com/example/slackclown/service/UserService.java
@@ -1,7 +1,13 @@
 package com.example.slackclown.service;
 
+import com.example.slackclown.dto.EmailDto;
+import com.example.slackclown.dto.ResetPasswordDto;
 import com.example.slackclown.dto.UserDto;
 
 public interface UserService {
     Object createUser(UserDto userDto);
+
+    Object sendOtp(EmailDto emailDto);
+
+    Object resetPassword(ResetPasswordDto resetPasswordDto);
 }
